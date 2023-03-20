23c8828962540b4f963721df67a43b840f119d8a
Tawfiq CADI TAZI <tawfiq@caditazi.fr>
2023-03-20 22:09:46 +0100

    Create symfony_installer.sh

---
+++ symfony_installer.sh
@@ -0,0 +1,93 @@
+#!/bin/bash -eH
+
+# Define colors
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+# Mises à jour
+echo -e "${YELLOW}Mises à jour${NC}"
+sudo apt update && sudo apt -y upgrade
+
+# Ajout des paquets
+echo -e "${YELLOW}Ajout des paquets${NC}"
+# Ajout des paquets
+echo -e "${YELLOW}Ajout des paquets${NC}"
+sudo apt update
+sudo apt -y install unixodbc lsb-release ca-certificates apt-transport-https software-properties-common
+sudo add-apt-repository -y ppa:ondrej/php
+sudo apt update
+
+
+# Installation de PHP 8.2
+echo -e "${YELLOW}Installation de PHP 8.2${NC}"
+sudo apt -y install php8.2-cli
+
+# Installation de composer
+echo -e "${YELLOW}Installation de composer (https://getcomposer.org/download/) ${NC}"
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+sudo mv composer.phar /usr/local/bin/composer
+
+
+# Installation de symfony-cli
+echo -e "${YELLOW}Installation de symfony-cli ()${NC}"
+curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh' | sudo -E bash
+sudo apt install symfony-cli
+
+
+# Instllation des extensions PHP
+echo -e "${YELLOW}Instllation des extensions PHP${NC}"
+sudo apt install php8.2-redis php8.2-amqp php8.2-pgsql php8.2-mysql php8.2-dev php8.2-xml php8.2-mbstring php8.2-xsl php8.2-xdebug php8.2-curl php8.2-zip php8.2-gd php8.2-intl -y --allow-unauthenticated
+
+# Installation de nodejs et yarn
+echo -e "${YELLOW}Installation de nodejs et yarn${NC}"
+
+# Instllation de nvm
+echo -e "${YELLOW}Instllation de nvm${NC}"
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+# Installation de nodejs
+echo -e "${YELLOW}Installation de nodejs${NC}"
+command -v nvm || echo -e "${RED}Erreur : nvm n'est pas installé.${NC}"
+nvm ls-remote
+
+echo -e "${GREEN}Entrez la version de node à installer :${NC}"
+read node_version
+
+nvm install $node_version
+nvm use $node_version
+# Installation de Yarn
+echo -e "${YELLOW}Installation de Yarn${NC}"
+npm install --global yarn
+
+# Vérifier que notre environnement nous permet de travailler avec Symfony
+echo -e "${YELLOW}Vérifier que notre environnement nous permet de travailler avec Symfony${NC}"
+echo -e "${YELLOW}Vérification de l'environnement pour Symfony${NC}"
+symfony check:requirements
+
+# Installation des drivers Sql Server
+
+echo -e "${YELLOW}Installation des drivers Sql Server${NC}"
+
+
+if ! pecl list | grep -q 'sqlsrv'; then
+    sudo pecl install sqlsrv
+fi
+
+if ! pecl list | grep -q 'pdo_sqlsrv'; then
+    sudo pecl install pdo_sqlsrv
+fi
+echo "; priority=20\nextension=sqlsrv.so\n" | sudo tee /etc/php/8.2/mods-available/sqlsrv.ini >/dev/null
+echo "; priority=30\nextension=pdo_sqlsrv.so\n" | sudo tee /etc/php/8.2/mods-available/pdo_sqlsrv.ini >/dev/null
+
+echo -e "${YELLOW}Installation des extensions PHP pour Sql Server${NC}"
+sudo phpenmod -v 8.2 sqlsrv pdo_sqlsrv
+
+echo -e "${GREEN}Le script est terminé !${NC}"
