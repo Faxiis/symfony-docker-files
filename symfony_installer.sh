58106c1548c58b4214dfe6b8407293d44c4be5ec
Tawfiq CADI TAZI <tawfiq@caditazi.fr>
2023-03-20 23:21:52 +0100

    Update symfony_installer.sh

--- symfony_installer.sh
+++ symfony_installer.sh
@@ -6,43 +6,58 @@ YELLOW='\033[0;33m'
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+# Define versions
+PHP_VERSION=8.2
+NODE_VERSION=16.14.0
+
 # Mises à jour
 echo -e "${YELLOW}Mises à jour${NC}"
 sudo apt update && sudo apt -y upgrade
 
 # Ajout des paquets
 echo -e "${YELLOW}Ajout des paquets${NC}"
-# Ajout des paquets
-echo -e "${YELLOW}Ajout des paquets${NC}"
 sudo apt update
 sudo apt -y install lsb-release ca-certificates apt-transport-https software-properties-common
-sudo add-apt-repository -y ppa:ondrej/php
-sudo apt update
 
 
-# Installation de PHP 8.2
-echo -e "${YELLOW}Installation de PHP 8.2${NC}"
-sudo apt -y install php8.2-cli
+# Ajout du dépôt PHP
+echo -e "${YELLOW}Ajout du dépôt PHP${NC}"
+if ! grep -q "ondrej/php" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+    sudo add-apt-repository -y ppa:ondrej/php
+    sudo apt update
+fi
+
+
+# Installation de PHP
+echo -e "${YELLOW}Installation de PHP ${PHP_VERSION}${NC}"
+if ! command -v php${PHP_VERSION} >/dev/null; then
+    sudo apt -y install php${PHP_VERSION}-cli
+fi
 
 # Installation de composer
 echo -e "${YELLOW}Installation de composer (https://getcomposer.org/download/) ${NC}"
-
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-sudo mv composer.phar /usr/local/bin/composer
+if ! command -v composer >/dev/null; then
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    php composer-setup.php
+    php -r "unlink('composer-setup.php');"
+    sudo mv composer.phar /usr/local/bin/composer
+fi
 
 
 # Installation de symfony-cli
 echo -e "${YELLOW}Installation de symfony-cli ()${NC}"
-curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh' | sudo -E bash
-sudo apt install symfony-cli
+if ! command -v symfony >/dev/null; then
+    curl -1sLf 'https://dl.cloudsmith.io/public/symfony/stable/setup.deb.sh' | sudo -E bash
+    sudo apt install symfony-cli
+fi
 
 
-# Instllation des extensions PHP
-echo -e "${YELLOW}Instllation des extensions PHP${NC}"
-sudo apt install php8.2-redis php8.2-amqp php8.2-pgsql php8.2-mysql php8.2-dev php8.2-xml php8.2-mbstring php8.2-xsl php8.2-xdebug php8.2-curl php8.2-zip php8.2-gd php8.2-intl -y --allow-unauthenticated
+# Installation des extensions PHP
+echo -e "${YELLOW}Installation des extensions PHP${NC}"
+sudo apt -y install php${PHP_VERSION}-redis php${PHP_VERSION}-amqp php${PHP_VERSION}-pgsql php${PHP_VERSION}-mysql php${PHP_VERSION}-dev php${PHP_VERSION}-xml php${PHP_VERSION}-mbstring php${PHP_VERSION}-xsl php${PHP_VERSION}-xdebug php${PHP_VERSION}-curl php${PHP_VERSION}-zip php${PHP_VERSION}-gd php${PHP_VERSION}-intl
+
+
 
 # Installation de nodejs et yarn
 echo -e "${YELLOW}Installation de nodejs et yarn${NC}"
@@ -53,51 +68,56 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 
-# Installation de nodejs
-echo -e "${YELLOW}Installation de nodejs${NC}"
-command -v nvm || echo -e "${RED}Erreur : nvm n'est pas installé.${NC}"
-nvm ls-remote
+# Install Node.js
+echo -e "${YELLOW}Installing Node.js ${NODE_VERSION}${NC}"
+nvm install ${NODE_VERSION}
+nvm use ${NODE_VERSION}
 
-echo -e "${GREEN}Entrez la version de node à installer :${NC}"
-read node_version
-
-nvm install $node_version
-nvm use $node_version
 # Installation de Yarn
 echo -e "${YELLOW}Installation de Yarn${NC}"
 npm install --global yarn
 
-# Vérifier que notre environnement nous permet de travailler avec Symfony
-echo -e "${YELLOW}Vérifier que notre environnement nous permet de travailler avec Symfony${NC}"
-echo -e "${YELLOW}Vérification de l'environnement pour Symfony${NC}"
+
+# Verify environment for Symfony
+echo -e "${YELLOW}Verifying environment for Symfony${NC}"
 symfony check:requirements
 
 # Installation des drivers Sql Server
-
 echo -e "${YELLOW}Installation des drivers Sql Server${NC}"
-
 if ! [[ "18.04 20.04 22.04" == *"$(lsb_release -rs)"* ]];
 then
     echo "Ubuntu $(lsb_release -rs) is not currently supported.";
     exit;
 fi
-
+# Ajout de la clé de signature pour Microsoft
 sudo su -c "curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -"
-
+# Ajout du référentiel de paquets pour les drivers SQL Server
 sudo sh -c "curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list | tee /etc/apt/sources.list.d/mssql-release.list >/dev/null"
-
+# Mise à jour des informations des paquets
 sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
-# optional: for bcp and sqlcmd
-sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
-echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
-source ~/.bashrc
-# optional: for unixODBC development headers
-sudo apt-get install -y unixodbc-dev
 
+# Installation des drivers SQL Server ODBC
+if ! dpkg -s msodbcsql18 > /dev/null 2>&1; then
+    sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+fi
 
+# Installation des outils bcp et sqlcmd pour SQL Server
+if ! dpkg -s mssql-tools18 > /dev/null 2>&1; then
+    sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
+fi
 
+# Ajout du répertoire bin des outils SQL Server à la variable PATH
+if ! grep -q '/opt/mssql-tools18/bin' ~/.bashrc; then
+    echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+    source ~/.bashrc
+fi
 
+# Installation des en-têtes de développement unixODBC
+if ! dpkg -s unixodbc-dev > /dev/null 2>&1; then
+    sudo apt-get install -y unixodbc-dev
+fi
+
+# Installation des extensions SQL Server pour PHP s'ils ne sont pas déjà installés
 if ! pecl list | grep -q 'sqlsrv'; then
     sudo pecl install sqlsrv
 fi
@@ -106,12 +126,20 @@ if ! pecl list | grep -q 'pdo_sqlsrv'; then
     sudo pecl install pdo_sqlsrv
 fi
 
-sudo sh -c 'echo "; priority=20\nextension=sqlsrv.so\n" > /etc/php/8.2/mods-available/sqlsrv.ini'
-sudo sh -c 'echo "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/8.2/mods-available/pdo_sqlsrv.ini'
+# Configuration des extensions SQL Server pour PHP
+if [ ! -f /etc/php/8.2/mods-available/sqlsrv.ini ]; then
+    sudo sh -c 'echo "; priority=20\nextension=sqlsrv.so\n" > /etc/php/8.2/mods-available/sqlsrv.ini'
+fi
+
+if [ ! -f /etc/php/8.2/mods-available/pdo_sqlsrv.ini ]; then
+    sudo sh -c 'echo "; priority=30\nextension=pdo_sqlsrv.so\n" > /etc/php/8.2/mods-available/pdo_sqlsrv.ini'
+fi
+
+
 
 
 echo -e "${YELLOW}Installation des extensions PHP pour Sql Server${NC}"
-sudo phpenmod -v 8.2 sqlsrv pdo_sqlsrv
+sudo phpenmod -v ${PHP_VERSION} sqlsrv pdo_sqlsrv
 
 echo -e "${GREEN}Le script est terminé !${NC}"
 
